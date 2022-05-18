f4aa24162a7839d984ff2a28308df59762927cca
Ziyi Lin <cengfeng.lzy@alibaba-inc.com>
2022-05-18 19:50:00 +0800

    [ENC] Prepare docker for musl static lib
    Summary: Prepare tool chains in docker for compiling musl based static
    library.
    
    Test Plan: all tests pass
    
    Reviewers: lei.yul, jeffery.wsj, sanhong.lsh
    
    Issue: https://aone.alibaba-inc.com/task/41619151
    
    CR:
    https://code.aone.alibaba-inc.com/java-tee/JavaEnclave/codereview/8751549

--- tools/cicd/make.sh
+++ tools/cicd/make.sh
@@ -14,8 +14,12 @@ if [[ "$(docker images -q ${BUILD_IMAGE}:${BUILD_TAG} 2> /dev/null)" == "" ]]; t
   # Get the customized Graal VM from git@gitlab.alibaba-inc.com:graal/SGXGraalVM.git
   # This should be replaced to the offical version when all patches are accepted by the Graal community
   wget https://graal.oss-cn-beijing.aliyuncs.com/graal-enclave/JDK11-22.0.0/graalvm-enclave-22.0.0.tar
+  wget http://graal.oss-cn-beijing.aliyuncs.com/graal-enclave/x86_64-linux-musl-native.tgz
+  wget http://graal.oss-cn-beijing.aliyuncs.com/graal-enclave/zlib-1.2.12.tar.gz
   docker build -t ${BUILD_IMAGE}:${BUILD_TAG} .
   rm -f graalvm-enclave-22.0.0.tar
+  rm -f x86_64-linux-musl-native.tgz
+  rm -f zlib-1.2.12.tar.gz
 fi
 
 # test JavaEnclave's unit test cases and samples
